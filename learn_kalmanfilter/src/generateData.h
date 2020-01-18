e5a69b80b5fb3d3c0a8293a1ece8a99f358e6127
poifull10 <poifull10@gmail.com>
2020-01-18 22:21:50 +0900

    add gtest

--- learn_kalmanfilter/src/generateData.h
+++ learn_kalmanfilter/src/generateData.h
@@ -19,4 +19,23 @@ public:
 private:
   Eigen::MatrixXf A_;
 };
+
+class ConstantVelocityModel : public Model
+{
+public:
+  ConstantVelocityModel(size_t col, size_t row)
+    : vel(Eigen::MatrixXf::Random(row, col))
+  {
+  }
+
+  Eigen::MatrixXf getVelocity() const { return vel; }
+  Eigen::MatrixXf operator()(const Eigen::MatrixXf& x) override
+  {
+    assert(vel.rows() == x.rows());
+    return x + vel;
+  }
+
+private:
+  Eigen::MatrixXf vel;
+};
 } // namespace kf
---
+++ learn_kalmanfilter/tests/testModel.cpp
@@ -0,0 +1,7 @@
+#include "generateData.h"
+#include "gtest/gtest.h"
+
+TEST(ConstantVelocityModel, test_operator)
+{
+  EXPECT_TRUE(true);
+}
