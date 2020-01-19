21a45464a448c50c6ef90b449911213ab0b163d5
poifull10 <poifull10@gmail.com>
2020-01-19 23:29:27 +0900

    wip

--- project/learn_calibration/calibration/find_target.cpp
+++ project/learn_calibration/calibration/find_target.cpp
@@ -1,1 +1,9 @@
 #include "find_target.h"
+
+namespace calib
+{
+TargetFinder::TargetFinder() {}
+
+void TargetFinder::operator()(const cv::Mat& img) {}
+
+} // namespace calib
--- project/learn_calibration/calibration/find_target.h
+++ project/learn_calibration/calibration/find_target.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <opencv2/opencv.hpp>
+
 namespace calib
 {
 class TargetFinder
 {
+public:
+  TargetFinder();
+
+  void operator()(const cv::Mat& img);
+
+private:
 };
 } // namespace calib
